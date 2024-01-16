2ae7896f0870bd4fc4619195ddb48a6c3eb670b8
albe68 <albertpaulus836@gmail.com>
2024-01-16 23:46:51 +0530

    feat: connected to db and queries written to repository

---
+++ server/src/frameworks/database/connection.js
@@ -0,0 +1,24 @@
+import { createConnection } from "mysql2";
+import "dotenv/config";
+const password=process.env.PASSWORD;
+export const connection = createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: password,
+  database: "test",
+});
+
+const dbConnection = () => {
+
+  connection.connect((err) => {
+    if (err) {
+      console.log(`${err}`);
+      return;
+    } else {
+      console.log(`db connected`);
+    }
+  });
+};
+
+export default dbConnection;
